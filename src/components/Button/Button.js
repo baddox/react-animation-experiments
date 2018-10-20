fef2597bd0bef79f69dd937919ed99302ed6e790
Thomas Shaddox <wediddoit@gmail.com>
2018-10-19 23:47:20 -0700

    surely this isn't the best way to do this button animation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,19 +3,44 @@ import { PropTypes } from 'prop-types';
 import classnames from 'classnames';
 import makeBem from '../../utils/makeBem';
 import './Button.scss';
-import { compose, withState } from 'recompose';
+import { compose, withState, lifecycle } from 'recompose';
+import { Motion, spring } from 'react-motion';
 
 const bem = makeBem('Button');
 
-const Button = ({ selected, setSelected, children }) => {
+const Button = ({ selected, setSelected, target, children }) => {
   const modifier = selected ? 'selected-true' : 'selected-false';
+  const scale = target === 'big' ? 1.5 : target === 'small' ? 0.75 : 1.0;
   return (
-    <button
-      className={classnames(bem())}
-      onClick={() => setSelected(!selected)}
+    <Motion
+      // defaultStyle={{ progress: 0.0 }}
+      style={{
+        scale: spring(scale, {
+          stiffness: 180,
+          damping: 12,
+          precision: 0.05,
+        }),
+      }}
     >
-      <span className={bem('icon', modifier)}>♥</span> {children}
-    </button>
+      {({ scale }) => {
+        return (
+          <button
+            className={classnames(bem())}
+            onClick={() => setSelected(!selected)}
+          >
+            <span
+              className={bem('icon', modifier)}
+              style={{
+                transform: `scale(${scale})`,
+              }}
+            >
+              ♥
+            </span>{' '}
+            {children}
+          </button>
+        );
+      }}
+    </Motion>
   );
 };
 
@@ -30,5 +55,19 @@ Button.defaultProps = {
 export default Button;
 
 export const ButtonStateful = compose(
-  withState('selected', 'setSelected', false)
+  withState('selected', 'setSelected', false),
+  withState('target', 'setTarget', 'normal'),
+  lifecycle({
+    componentDidUpdate({ selected: oldSelected }) {
+      const { selected, setTarget } = this.props;
+      if (selected === oldSelected) {
+        return;
+      }
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      setTarget(selected ? 'big' : 'small');
+      this.timer = setTimeout(() => setTarget('normal'), 200);
+    },
+  })
 )(Button);
